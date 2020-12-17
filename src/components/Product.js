e62e40ce51b03ad1afd1d5dae878156805dedfb9
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2020-12-17 04:02:06 +0100

    adjusted css properties as screen-width changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,14 +10,15 @@ export default function Product(props) {
     const {id, price, img, title, inCart} = props.product;
 
     return (
-        <div className="containers">
           
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+            <ProductWrapper className="centetdfdfd"> 
+            {/* col-9 mx-auto col-md-6 col-lg-3 my-3 */}
+           
                <ProductConsumer>
                    {value =>{
                        return(
                             <div className="display-flex">
-                
+                                <div style={{marginTop: '6rem'}}></div>
                             <div className="products">
                                     <div className="product-name">
                                         <h2>{title}</h2>
@@ -51,7 +52,6 @@ export default function Product(props) {
                </ProductConsumer>
             </ProductWrapper>
 
-        </div>
     )
 }
 
@@ -60,19 +60,12 @@ const ProductWrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-bottom: 2rem;
-  /* z-index: -1; */
  
-    /* .display-flex{
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-    } */
     .products{
         box-shadow: .5px 1px 2px 1.5px  grey;
         width: 21rem;
         transition: 1s all ease;
-        /* &:hover{
-          transform: scale(1.05);
-        } */
+       
     }
     .image-container{
         transition: 1s all ease;
--- src/components/ProductMenu.js
+++ src/components/ProductMenu.js
@@ -5,16 +5,18 @@ import '../App.css'
 
 
 
-export default function Productmenu(props) {
+export default function Productmenu() {
     
     return (
-        <div>
-            <div style={{marginTop: '10rem'}}></div>
+        <div
+            className="change-display"
+        >
+            
             <ProductConsumer>
                {value=>(
                    value.products.map(product =>(
 
-                        <Product key={product.id} product={product} />
+                            <Product key={product.id} product={product} />
                         
 
                    ))
