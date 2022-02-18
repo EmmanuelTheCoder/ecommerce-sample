084411c54590f9df6a5329c46c9ad9e2b7f5d126
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-02-18 14:14:19 +0100

    adding cart values

--- src/components/cartItems.js
+++ src/components/cartItems.js
@@ -1,16 +1,35 @@
+import '../App.css';
 
 export default function CartItems({items}) {
     const {title, img, price, } = items
   return (
     <div>
-        <h1>Cart items listed</h1>
-        <div>
-            <h1>Item</h1>
-            <h1>Name of item</h1>
+        <div className="cart-item">
+            <div>
+                <h1>Item</h1>
+
+            </div>
+
+            <div>
+
+                <h1>Name of item</h1>
+            </div>
+            <div>
             <h1>price</h1>
-            <h1>quantity</h1>
-            <h1>remove</h1>
-            <h1>total</h1>
+
+            </div>
+            <div>
+
+                <h1>quantity</h1>
+            </div>
+            <div>
+
+                <h1>remove</h1>
+            </div>
+            <div>
+                <h1>total</h1>
+
+            </div>
         </div>
     </div>
   )
