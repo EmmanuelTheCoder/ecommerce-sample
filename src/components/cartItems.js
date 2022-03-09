2b7b7c51a2efdf6440f5c16e747f6b13f7d86df7
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-03-09 14:49:58 +0100

    solving when array is empty

--- src/components/cartItems.js
+++ src/components/cartItems.js
@@ -5,20 +5,26 @@ import '../App.css';
 export default function CartItems({items}) {
     //console.log("items to be displayed", items)
 
-    const displayItems = () =>{
-      items.map(item =>{
-        const {title, img, price, count, total} = item
-        return(
-          <div key={item.id}>
-              <img src={img} alt="cart product" />
-          </div>
-        )
-      })
+    const DisplayItems = () =>{
+      if(items !== undefined){
+
+        return items.map(item =>{
+          const {title, img, price, count, total} = item
+          console.log("items to be displayed", item)
+          return(
+            <div key={item.id}>
+                <img src={img} alt="cart product" />
+            </div>
+          )
+        })
+      }else if (items.length === 0){
+        return <h1>Cart is currently empty</h1>
+      }
 
     }
   return (
     <div className='cartitem-container'>
-       {displayItems}
+      <DisplayItems /> 
     </div>
   );
 }
