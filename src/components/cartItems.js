3f7caf6a0ac699b0ad2092f20b14b9111c3dd0a7
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-03-09 15:35:49 +0100

    autoprefixer warning

--- src/components/cartItems.js
+++ src/components/cartItems.js
@@ -17,12 +17,13 @@ export default function CartItems({items}) {
   
   const DisplayItems = () =>{
       if(items.length === 0){
-        return <h1 style={{textAlign: 'center', fontSize: '1.1rem', fontStyle: 'italic'}}>Cart is currently empty</h1>
+        return <h1 style={{textAlign: 'center', fontSize: '2.5rem', fontFamily: 'Montserrat', fontStyle: 'italic'}}>Cart is currently empty</h1>
       }
       if(items !== undefined){
 
         return items.map(item =>{
-          const {title, img, price, count, total} = item
+         const {title, img, price, count, total} = item
+         
           console.log("items to be displayed", item)
           return(
             <div key={item.id}>
