c401d678592673724b442381a44efe2e6724f3a0
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2022-03-18 00:17:00 +0100

    battling with css

--- src/components/cartItems.js
+++ src/components/cartItems.js
@@ -5,13 +5,44 @@ export default function CartItems({items}) {
   const ListHeader = () =>{
     return(
       <div className="cart-list-header">
-        <h1>Product</h1>
-        <h1 className="display-none">Product name</h1>
-        <h1>price</h1>
-        <h1>quantity</h1>
-        <h1>remove</h1>
-        <h1>total</h1>
+        <div className="sub-cart-list-header">
+          <h1>Product</h1>
+          <h1 className="display-none">Product name</h1>
+          <h1>price </h1>
+          <h1>quantity</h1>
+          <h1>remove</h1>
+          <h1>total</h1>
+
+        </div>
+        {items.length === 0 ? <h1 style={{textAlign: 'center', fontSize: '2.5rem', fontFamily: 'Montserrat', fontStyle: 'italic'}}>
+        Cart is currently empty
+        </h1> : ""}
+
+        {items !== undefined ?
+          items.map(item => {
+            const {title, img, price, count, total} = item
+            return(
+              <div key={item.id} className="cart-product-list">
+                  <img src={img} alt="cart product" className="cart-product-img"/>
+                  <p>{title}</p>
+                  <p> <strong>{price}</strong></p>
+                  <p className='btn'><strong className='toggle-count'>-</strong>{count} 
+                  <strong className='toggle-count'>+</strong>
+                  </p>
+                  <img src="https://img.icons8.com/ios-glyphs/30/fa314a/filled-trash.png"
+                  className='fa-del'
+                  alt='delete'
+                  />
+                  <p><strong></strong>${total}</p>
+                  {/* <a href="https://icons8.com/icon/67884/delete">Delete icon by Icons8</a> */}
+              </div>
+
+            )
+          }) : ""
+      
+      }
       </div>
+    
     )
   }
   
@@ -33,7 +64,7 @@ export default function CartItems({items}) {
                 <p className='btn'><strong className='toggle-count'>-</strong>{count} 
                 <strong className='toggle-count'>+</strong>
                 </p>
-                <img src="https://img.icons8.com/ios-glyphs/60/fa314a/filled-trash.png"
+                <img src="https://img.icons8.com/ios-glyphs/30/fa314a/filled-trash.png"
                 className='fa-del'
                 alt='delete'
                 />
@@ -49,7 +80,7 @@ export default function CartItems({items}) {
   return (
     <div className='cartitem-container'>
       {items.length > 0 ? <ListHeader /> : ""}
-      {/* <DisplayItems />  */}
+      
     </div>
   );
 }
